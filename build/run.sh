80b88fe5b7da3f8563c2141da4781adfd884727a
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-12-19 12:49:39 -0500

    Fixing run.sh to use new directory structure

--- build/run.sh
+++ build/run.sh
@@ -15,10 +15,10 @@ sleep 10;
 
 pwd=$(pwd)
 
-minikube ssh "cd ${pwd} && ./util/build.sh"
+minikube ssh "cd ${pwd} && ./build/build.sh"
 
-kubectl apply -f infrastructure/flask-deployment.yaml
-kubectl apply -f infrastructure/flask-service.yaml
-kubectl apply -f infrastructure/nginx-configmap.yaml
-kubectl apply -f infrastructure/nginx-deployment.yaml
-kubectl apply -f infrastructure/nginx-service.yaml
+kubectl apply -f local-test/flask-deployment.yaml
+kubectl apply -f local-test/flask-service.yaml
+kubectl apply -f local-test/nginx-configmap.yaml
+kubectl apply -f local-test/nginx-deployment.yaml
+kubectl apply -f local-test/nginx-service.yaml
